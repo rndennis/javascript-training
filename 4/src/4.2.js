f32b6e872299b7c6e13ec9e23ffefe4c690255ae
Renee Dennis <reneenicoledennis@gmail.com>
2018-02-26 07:56:52 -0600

    Remove solution from 4.2.js

--- 4/src/4.2.js
+++ 4/src/4.2.js
@@ -1,12 +1,11 @@
 import fetch from 'node-fetch'; // Fetch API for Node.js
 
 // TODO: Import the url from resources/api.js
-import { EARTHQUAKES } from './resources/api';
 
 class IcelandApi {
   static getEarthquakes() {
     // TODO: Uncomment the following line and provide the URL as a parameter to fetch()
-    return fetch(EARTHQUAKES).then(response => response.json());
+    // return fetch().then(response => response.json());
   }
 }
 
@@ -15,16 +14,8 @@ IcelandApi.getEarthquakes().then(({ results }) => {
   earthquakes = results;
 });
 
-// TODO: Get the number of earthquakes by day
-const numEarthquakesByDay = earthquakes.reduce((accumulator, currentValue) => {
-  const day = new Date(currentValue.timestamp).toDateString();
-  if (accumulator[day]) {
-    accumulator[day] += 1;
-  } else {
-    accumulator[day] = 1;
-  }
-  return accumulator;
-}, {});
+// TODO: Get the number of earthquakes by day (hint: try doing it with Array.prototype.reduce())
+// let numEarthquakesByDay;
 
 console.log('earthquakes:', earthquakes);
 console.log(
