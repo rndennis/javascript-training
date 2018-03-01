496652d8a9f71d81f80d7c09030f0028b97340a3
Renee Dennis <reneenicoledennis@gmail.com>
2018-03-01 11:22:10 -0600

    Remove compiled code

--- 4/dist/resources/named.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var movies = exports.movies = ['Forrest Gump', 'Interstellar', 'Titanic', 'La La Land', 'Toy Story 2'];
-
-var tvShows = exports.tvShows = ['How To Get Away With Murder', 'Suits', 'The Office', 'Stranger Things', 'Parks and Recreation'];
